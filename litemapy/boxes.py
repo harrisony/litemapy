758010acad3c9520ae79b3d28b6954ed8dc6b901
Harrison Conlin <me@harrisony.com>
2026-02-14 12:08:26 +1100

    fix: correct z-coordinate extraction in block_is_in_box
    Fixed bug where z-coordinates were incorrectly extracted from y-coordinates
    in the box tuple (box[0][1], box[1][1] instead of box[0][2], box[1][2]).
    This caused incorrect box containment checks for z-axis.

--- litemapy/boxes.py
+++ litemapy/boxes.py
@@ -12,7 +12,7 @@ def block_is_in_box(block: Coordinate, box: tuple[Coordinate, Coordinate]) -> bo
     xs.sort()
     ys = [box[0][1], box[1][1]]
     ys.sort()
-    zs = [box[0][1], box[1][1]]
+    zs = [box[0][2], box[1][2]]
     zs.sort()
     x_min, x_max = xs
     y_min, y_max = ys
@@ -20,5 +20,7 @@ def block_is_in_box(block: Coordinate, box: tuple[Coordinate, Coordinate]) -> bo
     return x_min <= x <= x_max and y_min <= y <= y_max and z_min <= z <= z_max
 
 
-def box_is_in_box(box1: tuple[Coordinate, Coordinate], box2: tuple[Coordinate, Coordinate]) -> bool:
+def box_is_in_box(
+    box1: tuple[Coordinate, Coordinate], box2: tuple[Coordinate, Coordinate]
+) -> bool:
     return block_is_in_box(box1[0], box2) and block_is_in_box(box1[1], box2)
